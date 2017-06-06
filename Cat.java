5f5eeadc2d53098ebd98aa16939f2b2c3039df71
IKostrytsia <kostritsa.es@gmail.com>
2017-06-06 16:01:07 +0300

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,25 @@
+public class Cat {
+    Integer weight;
+    String name;
+
+    public Cat(Integer weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
