e207c548d34dd9c56f11ec3351b23790335b21de
IKostrytsia <kostrytsia.es@gmail.com>
2017-06-07 11:28:57 +0300

    Animals coomit

---
+++ Animals.java
@@ -0,0 +1,7 @@
+public class MakePets {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat(10, "Tom");
+      Cat cat1 = new Cat(20, "Mark");
+        System.out.println(cat1);
+    }
+}
